ff925aaeea5bcf197f7fd15416943c6095ac7327
Michael Adams <mtadams007@gmail.com>
2018-06-21 15:14:04 -0400

    built a basic api

---
+++ CalendarBackend/db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create(start:'3:00', end:'4:00', description: "This is when Spotify hires Michael", date:"2018-7-1")
