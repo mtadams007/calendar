bdfaa87224cc7c68a44fbf5d14f3aba1ad145c6a
Michael Adams <mtadams007@gmail.com>
2018-06-24 17:19:54 -0400

    added spotify colors and days of the week

--- CalendarFrontEnd/src/App.js
+++ CalendarFrontEnd/src/App.js
@@ -285,6 +285,9 @@ class App extends Component {
           {eventList}
         </div>
         <div className="content">
+          <div className="dayName">Sunday</div><div className="dayName">Monday</div><div className="dayName">Tuesday</div>
+          <div className="dayName">Wednesday</div><div className="dayName">Thursday</div><div className="dayName">Friday</div>
+          <div className="dayName">Saturday</div>
           {this.renderCalendar(this.state.monthNumber)}
         </div>
         <div id="form">
